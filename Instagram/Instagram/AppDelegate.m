3550173a944ddf6c6330264808297785d1700db2
Lily Yang <lilyyang@fb.com>
2022-06-27 15:04:25 -0700

    Created signup and login

--- Instagram/Instagram/AppDelegate.m
+++ Instagram/Instagram/AppDelegate.m
@@ -27,19 +27,6 @@
     }];
 
     [Parse initializeWithConfiguration:config];
-    
-    // Test working!
-    PFObject *gameScore = [PFObject objectWithClassName:@"GameScore"];
-    gameScore[@"score"] = @1337;
-    gameScore[@"playerName"] = @"Sean Plott";
-    gameScore[@"cheatMode"] = @NO;
-    [gameScore saveInBackgroundWithBlock:^(BOOL succeeded, NSError *error) {
-        if (succeeded) {
-            NSLog(@"Object saved!");
-        } else {
-            NSLog(@"Error: %@", error.description);
-        }
-    }];
     return YES;
 }
 
